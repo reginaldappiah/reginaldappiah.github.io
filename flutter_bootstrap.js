50e1414387e18eb011493c973bb53c319c73b6e0
reginaldappiah <reginaldappiah@users.noreply.github.com>
2025-03-24 13:53:30 +0000

    deploy: reginaldappiah/futurefleetatl@431896c60eea86534e8439fb6b683ae937c0a1a9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "395219428"
+    serviceWorkerVersion: "2403610055"
   }
 });
