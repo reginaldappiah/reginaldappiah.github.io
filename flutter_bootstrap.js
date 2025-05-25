d2e6f3d9294994e6b9b0b840eeefcaf22a8dcead
Reginald Appiah <reginald.appiah7@gmail.com>
2025-05-25 10:42:11 -0400

    Deploy 2025-05-25 10:41

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4175700988"
+    serviceWorkerVersion: "2723201297"
   }
 });
